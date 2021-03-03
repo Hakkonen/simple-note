e8c1231c75841d51ff0e982ada8b53ac079cda3d
hakkonen <rnlds.j@gmail.com>
2021-03-03 13:49:05 +1100

    v0.1.4
    Resolved portrait and landscape styling

--- simple-note-react/src/App.js
+++ simple-note-react/src/App.js
@@ -1,48 +1,15 @@
 import React, { useState } from "react"
 import NotePage from "./components/NotePage"
 import IndexItem from "./components/IndexItem"
+import { NoteDB } from "./data/NoteDB"
 import './App.css'
 
 function App() {
   const [ notes, setNotes ] = useState([
-    {
-        index: 1,
-        header: "World, hello!",
-        text: "Hello world"
-    },
-    {
-        index: 2,
-        header: "'Ello there guvna",
-        text: "Test note"
-    },
-    {
-        index: 3,
-        header: "My note",
-        text: "Pop a testie"
-    },
-    {
-      index: 4,
-      header: "Lorem ipsum",
-      text: "Test note"
-    },
-    {
-      index: 5,
-      header: "Lorem ipsum",
-      text: "Test note"
-    },
-    {
-      index: 6,
-      header: "Lorem ipsum",
-      text: "Test note"
-    },
-    {
-      index: 7,
-      header: "Lorem ipsum",
-      text: "Test note"
-    },
+    
   ])
 
-  const printNotes = notes.map(item => <IndexItem key={item.index} index={item.index} header={item.header} text={item.text} />)
+  const printNotes = NoteDB.map(item => <IndexItem key={item.index} index={item.index} header={item.header} text={item.text} />)
 
   return (
     <div className="app-container">
---
+++ simple-note-react/src/data/NoteDB.js
@@ -0,0 +1,37 @@
+export const NoteDB = [
+    {
+        index: 1,
+        header: "World, hello!",
+        text: "Hello world"
+    },
+    {
+        index: 2,
+        header: "Hello there guvna what's it about",
+        text: "Test note"
+    },
+    {
+        index: 3,
+        header: "My note",
+        text: "Pop a testie"
+    },
+    {
+        index: 4,
+        header: "Lorem ipsum",
+        text: "Test note"
+    },
+    {
+        index: 5,
+        header: "Lorem ipsum",
+        text: "Test note"
+    },
+    {
+        index: 6,
+        header: "Lorem ipsum",
+        text: "Test note"
+    },
+    {
+        index: 7,
+        header: "Lorem ipsum",
+        text: "Test note"
+    }
+]
